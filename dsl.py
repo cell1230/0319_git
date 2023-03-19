0ddd31fd99bf3c0584dbba93966107b57a622e0c
cell1231 <lynn0804@yonsei.ac.kr>
2023-03-19 15:40:07 +0900

    add cow

--- dsl.py
+++ dsl.py
@@ -6,6 +6,8 @@ class dsl(object):
             print(f'{self.name} is a cat')
         elif self.name == 'dog':
             print(f'{self.name}is a dog')
+        elif self.name == 'cow':
+            print(f'{self.name}is a cow')
         else:
             print(f'{self.name}가 최고다')
 
